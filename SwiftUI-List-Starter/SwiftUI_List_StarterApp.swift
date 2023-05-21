d5ce4d4077976c29e7be344c0f30846b58afbc45
Oleg Plugaru <oleg.plugaru.md@gmail.com>
2023-05-21 20:07:53 +0300

    Initial Commit

---
+++ SwiftUI-List-Starter/SwiftUI_List_StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_List_StarterApp.swift
+//  SwiftUI-List-Starter
+//
+//  Created by Whoami on 21.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_List_StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
